5e0e71597954029862cd4162d091e64c7cb7d610
ftn444 <farreltn4@gmail.com>
2026-09-08 08:06:33 -0700

    feat(core): initialize artisan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        echo "<?php echo 'Test';?>"
+        <?php echo 'Test';?>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
